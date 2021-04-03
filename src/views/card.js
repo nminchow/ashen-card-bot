93e448fae7f1b6c897058da62bdd783db4599209
nminchow <noelminchow@gmail.com>
2021-04-03 18:13:50 -0400

    update images

--- src/views/card.js
+++ src/views/card.js
@@ -3,9 +3,10 @@ const Discord = require('discord.js');
 module.exports = ({ name, stub, dice }, fullArt) => {
   const url = `https://ashes.live/cards/${stub}/`;
   const imgUrl = `https://cdn.ashes.live/images/cards/${stub}.jpg`;
-  const diceUrl = `https://cdn.ashes.live/images/cards/${dice}-magic.jpg`;
+  const diceUrl = `https://nminchow.github.io/ashen-card-bot/dice/${dice}.png`;
   const embed = new Discord.MessageEmbed();
-  embed.setAuthor(name, diceUrl, url);
+  embed.setFooter(null, diceUrl);
+  embed.setAuthor(name, null, url);
   if (fullArt) {
     embed.setImage(imgUrl);
   } else {
