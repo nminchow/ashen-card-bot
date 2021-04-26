8e939a9e5d7e7cbac7b1da70dcd0d85549afc54a
nminchow <noelminchow@gmail.com>
2021-04-26 19:43:05 -0400

    fix deck error handling and lint fixes

--- src/controllers/decks/fetch.js
+++ src/controllers/decks/fetch.js
@@ -2,14 +2,14 @@ const got = require('got');
 const deck = require('../../views/deck');
 
 const sendIfFound = (message, result) => {
-  if (result) {
+  if (result.id) {
     return message.channel.send({ embed: deck(result, message.client) });
   }
   return message.channel.send('deck not found');
 };
 
 const byUUID = async (message, uuid) => {
-  const result = await got(`https://api.ashes.live/v2/decks/shared/${uuid}`).json();
+  const result = await got(`https://api.ashes.live/v2/decks/shared/${uuid}`).json().catch(() => ({}));
   return sendIfFound(message, result);
 };
 
@@ -17,7 +17,7 @@ const byId = async (message, id) => {
   if (Number.isNaN(Number(id))) {
     return byUUID(message, id);
   }
-  const { deck, releases } = await got(`https://api.ashes.live/v2/decks/${id}`).json();
+  const { deck, releases } = await got(`https://api.ashes.live/v2/decks/${id}`).json().catch(() => ({}));
   return sendIfFound(message, { releases, ...deck });
 };
 
