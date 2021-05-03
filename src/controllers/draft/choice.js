9a766176775cefc0c96fc1fccbe301216c0e942b
nminchow <noelminchow@gmail.com>
2021-05-02 20:20:07 -0400

    implement phoenixborn choice rotation

--- src/controllers/draft/choice.js
+++ src/controllers/draft/choice.js
@@ -1,3 +1,4 @@
+const { pull } = require('lodash');
 const choice = require('../../views/draft/choice');
 
 module.exports = async ({ emoji: { name } }, user, draftSnapshot, enabled) => {
@@ -10,13 +11,7 @@ module.exports = async ({ emoji: { name } }, user, draftSnapshot, enabled) => {
       return dm.send("Whoops - looks like it isn't your turn");
     }
   }
-
-  if (playerData.subSelection) {
-    // user has made subselection
-  }
-
   if (draft.pools.stage === 'phoenixborn') {
-    console.log('handling initial reaction');
     const { emojiMapping } = choice(draftSnapshot.ref.id, user.id, user.client, draft);
     const result = emojiMapping[name];
     console.log(result);
@@ -30,10 +25,16 @@ module.exports = async ({ emoji: { name } }, user, draftSnapshot, enabled) => {
       // just normal phoenixborn set
       playerData.subSelection = null;
       playerData.phoenixborn = result;
-      // TODO: figure out how we do this
-      // draft.pools.current =
+      pull(draft.pools.phoenixborn, result);
+      const nextPlayer = draft.players.find((id) => draft.playerData[id].phoenixborn === null);
+      if (!nextPlayer) {
+        draft.pools.stage = 'card';
+        // eslint-disable-next-line prefer-destructuring
+        draft.pools.current = draft.players[0];
+      } else {
+        draft.pools.current = nextPlayer;
+      }
     }
     return draftSnapshot.ref.set(draft);
-    // build view and check if emoji mapping is an array, if it is an array, user is making subselection, if not, they are making actual selection
   }
 };
